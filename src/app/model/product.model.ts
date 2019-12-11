7790ef23854ed29e6f1c8dbe23729d1f318a2558
pedronso <pedrocaetanoaraujopc@gmail.com>
2019-12-11 08:32:33 -0300

    first commit

---
+++ src/app/model/product.model.ts
@@ -0,0 +1,11 @@
+export class Product {
+
+    constructor(
+        //adicionar atributo icon
+        public id?: number,
+        public name?: string,
+        public category?: string,
+        public description?:string,
+        public price?: number) {}
+
+}
