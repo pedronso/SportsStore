26f8668d827c5a170b9ee140bac3dd1b68abd357
pedronso <pedrocaetanoaraujopc@gmail.com>
2019-12-11 09:34:05 -0300

    adicona imagens

--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -6,6 +6,7 @@ export class Product {
         public name?: string,
         public category?: string,
         public description?:string,
-        public price?: number) {}
+        public price?: number,
+        public src? : any ) {}
 
 }
